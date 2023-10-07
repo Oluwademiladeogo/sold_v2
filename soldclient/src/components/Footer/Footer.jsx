bcbe6ba1f57d0ed9f7a47e7b67de31e56763d211
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-07 13:49:44 +0100

    Feat: added footer

---
+++ soldclient/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+const Footer = ()=>{
+    return (
+        <>
+        <footer>
+            <p>
+                Contact
+            </p>
+        </footer>
+        </>
+    )
+}
+export default Footer;
