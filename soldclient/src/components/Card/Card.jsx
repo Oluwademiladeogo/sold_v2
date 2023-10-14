a1a506bb51b40b2b41c04bbbad75939d0f02e02e
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-14 13:43:48 +0100

    modified cards

--- soldclient/src/components/Card/Card.jsx
+++ soldclient/src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import "./Card.css";
 const Card = (props) => {
   return (
     <>
-      {/* <div className="grid"> */}
+    
         <div className="card__body item">
           <img
             className="card__img"
@@ -15,59 +15,6 @@ const Card = (props) => {
             <a href="cardnum" className="card__price">{props.price}</a>
           </div>
         </div>
-
-        <div className="card__body item">
-          <img
-            className="card__img"
-            src={props.card_img}
-            alt={props.card_img_alt}
-          />
-          <div className="card__text">
-            <div className="card__name">{props.name}</div>
-            <div className="card__description"> {props.description}</div>
-            <a href="cardnum" className="card__price">{props.price}</a>
-          </div>
-        </div>
-
-        <div className="card__body item">
-          <img
-            className="card__img"
-            src={props.card_img}
-            alt={props.card_img_alt}
-          />
-          <div className="card__text">
-            <div className="card__name">{props.name}</div>
-            <div className="card__description"> {props.description}</div>
-            <a href="cardnum" className="card__price">{props.price}</a>
-          </div>
-        </div>
-
-        <div className="card__body item">
-          <img
-            className="card__img"
-            src={props.card_img}
-            alt={props.card_img_alt}
-          />
-          <div className="card__text">
-            <div className="card__name">{props.name}</div>
-            <div className="card__description"> {props.description}</div>
-            <a href="cardnum" className="card__price">{props.price}</a>
-          </div>
-        </div>
-
-        <div className="card__body item">
-          <img
-            className="card__img"
-            src={props.card_img}
-            alt={props.card_img_alt}
-          />
-          <div className="card__text">
-            <div className="card__name">{props.name}</div>
-            <div className="card__description"> {props.description}</div>
-            <a href="cardnum" className="card__price">{props.price}</a>
-          </div>
-        </div>
-      {/* </div> */}
     </>
   );
 };
