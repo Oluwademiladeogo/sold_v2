757c01105eba1f8258fa2559431743f562033418
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-14 13:45:01 +0100

    component for fontlinks

---
+++ soldclient/src/components/FontLinks/FontLinks.jsx
@@ -0,0 +1,29 @@
+const FontLinks = () => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        rel="stylesheet"
+      />
+      <script src="https://kit.fontawesome.com/86988e59cc.js" crossorigin="anonymous"></script>
+
+    </>
+  );
+};
+export default FontLinks
