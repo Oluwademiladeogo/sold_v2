358f5d014f235bb9961b5a5f380607c8e79cb366
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-14 13:45:22 +0100

    add loading spinner

---
+++ soldclient/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+const Loading = () => {
+  return (
+    <>
+      {/* <dialog className="load" open>Loading...</dialog> */}
+      <div className="spinner-container" />
+      <div className="loading-spinner" />
+    </>
+  );
+};
+export default Loading;
