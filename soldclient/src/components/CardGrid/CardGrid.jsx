eac5703284b221ea37eaa80abfa3658c8b14cbbb
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-14 13:44:21 +0100

    specify card layout

--- soldclient/src/components/CardGrid/CardGrid.jsx
+++ soldclient/src/components/CardGrid/CardGrid.jsx
@@ -1,16 +1,53 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
+import Loading from "../Loading/Loading";
 import "./CardGrid.css";
 const CardGrid = (props) => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setisLoading(true);
+        setError("")
+        const response = await fetch("http://localhost:3000");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        } 
+        const data = await response.json();
+        if(data.Response === 'False') throw new Error("Data not found")
+        setItems(data.items);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+        setError(error.message);
+      } finally {
+        setisLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const ErrorMessage = ({ message }) => {
+    return <div className="error">message</div>;
+  };
   return (
     <>
       <div className="grid">
-        <Card
-          card_img={props.card_img}
-          card_img_alt={props.card_img_alt}
-          name={props.name}
-          description={props.description}
-          price={props.price}
-        ></Card>
+        {isLoading && <Loading />}
+        {!isLoading &&
+          !error &&
+          items.map((item) => (
+            <Card
+              key={item.id}
+              card_img={item.card_img}
+              card_img_alt={item.card_img_alt}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+            ></Card>
+          ))}
+        {error && <ErrorMessage message={error} />}
       </div>
     </>
   );
