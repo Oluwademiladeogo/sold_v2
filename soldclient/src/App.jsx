48e68ac7c149aede966b5f774f8c181d742ca3af
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-14 13:43:11 +0100

    minor changes

--- soldclient/src/App.jsx
+++ soldclient/src/App.jsx
@@ -1,22 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import CardGrid from "./components/CardGrid/CardGrid";
+import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
+import path from "path";
 function App() {
   const [count, setCount] = useState(0);
-  const name = "Airpods pro";
-  const description = "Description is very short";
-  const price = "$99.99";
+
   return (
     <>
-    <Navbar token="true"/>
-      <CardGrid
-        card_img="/card.jpg"
-        card_img_alt="bible"
-        name={name}
-        description={description}
-        price={price}
-      ></CardGrid>
+    {/* <Login/> */}
+      <Navbar token="true" />
+      <CardGrid />
     </>
   );
 }
