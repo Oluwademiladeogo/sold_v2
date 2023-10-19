4b3568b573dfb212ef3f2f51c1fe062dcbb5fc91
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-19 14:19:11 +0100

    Minor Ui changes

--- soldclient/src/components/Card/Card.jsx
+++ soldclient/src/components/Card/Card.jsx
@@ -1,20 +1,25 @@
 import "./Card.css";
 const Card = (props) => {
+  const cardClickHandler = () => {
+    window.location = `/${props.link}`;
+    console.log("clicked");
+  };
   return (
     <>
-    
-        <div className="card__body item">
-          <img
-            className="card__img"
-            src={props.card_img}
-            alt={props.card_img_alt}
-          />
-          <div className="card__text">
-            <div className="card__name">{props.name}</div>
-            <div className="card__description"> {props.description}</div>
-            <a href="cardnum" className="card__price">{props.price}</a>
-          </div>
+      <div className="card__body item" onClick={cardClickHandler}>
+        <img
+          className="card__img"
+          src={props.card_img}
+          alt={props.card_img_alt}
+        />
+        <div className="card__text">
+          <div className="card__name">{props.name}</div>
+          <div className="card__description"> {props.description}</div>
+          <a href="cardnum" className="card__price">
+            {props.price}
+          </a>
         </div>
+      </div>
     </>
   );
 };
