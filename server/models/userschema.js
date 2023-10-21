f2c40fcc919d26b683177d40c07363ccdcb8c0cc
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-21 11:08:55 +0100

    Added joi userschema for data validation

---
+++ server/models/userschema.js
@@ -0,0 +1,6 @@
+const joi = require("joi")
+module.exports = joi.object({
+    name: joi.string().min(4).max(100).required(),
+    email: joi.string().min(5).max(255).email().required(),
+    password: joi.string().min(8).max(255).required()
+})
