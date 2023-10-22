220f1367e7c1138d4c35bb5257aa73ab46dbfe92
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-22 23:58:52 +0100

    Navbat finally responsive

--- soldclient/src/components/Navbar/Navbar.jsx
+++ soldclient/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import { react, useState } from "react";
+import { react, reactRouter, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import "./Navbar.css";
+// import BootstrapLinks from "../BootstrapLinks/BootstrapLinks";
 import FontLinks from "../FontLinks/FontLinks";
 const Navbar = (props) => {
   // const [token, setToken] = useState("")
@@ -9,95 +11,43 @@ const Navbar = (props) => {
     console.log("clicked");
   };
   return (
-    <>
+    <nav>
       <FontLinks />
-      <div className="nav ">
-        {token === "true" ? (
-          <div>
-            <a className="nav__link__sold">SOLD</a>
-            <a href="#" className="nav__link">
-              HOME
-            </a>
-            {/* <a className="nav__link">QUALITY SERVICES AND DELIVERY</a> */}
-            <input
-              type="text"
-              name="search"
-              placeholder="Search"
-              className="nav__search"
-            ></input>
-            <button
-              type="submit"
-              className="nav__button"
-              onClick={buttonClickHandler}
-            >
-              {/* <i className="fa fa-search" /> */}
-            </button>
-            <a href="#" className="nav__link">
-              CART
-            </a>
-            <i className="fa-regular fa-user fa-2xs"></i>
-            <a href="#" className="nav__link">
-              CONTACT
-            </a>
-            <a href="#" className="nav__link">
-            <i className="fa-solid fa-user-lock fa-2xs"></i>
-            </a>
-            <a href="#" className="nav__link">
-              LOGOUT
-            </a>
-          </div>
-        ) : (
-          //
-          //
-          //
-          //
-          //
-          //
-          //
-          //
-          //
-          //
-          //Space added for code readability
-          //
-          //
-          //
-          <div>
-            <a className="nav__link__sold">SOLD</a>
-            <a href="#" className="nav__link">
-              HOME
-            </a>
-            <a href="#" className="nav__link">
-              ABOUTUS
-            </a>
-            <a href="#" className="nav__link">
-              CART
-            </a>
-            {/* <a className="nav__link">QUALITY SERVICES AND DELIVERY</a> */}
-            <input
-              type="text"
-              name="search"
-              placeholder="Search"
-              className="nav__search"
-            ></input>
-            <button
-              type="submit"
-              className="nav__button"
-              onClick={buttonClickHandler}
-            >
-              <i className="fa fa-search" />
-            </button>
-            <a href="#" className="nav__link">
-              REGISTER
-            </a>
-            <a href="#" className="nav__link">
-              SIGNUP
-            </a>
-          </div>
-        )}
-        {/* might want to do categories later */}
-        {/* isauth  */}
+      <Link to="/" className="title">
+        SOLD
+      </Link>
+      <div className="menu">
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    </>
+      <ul>
+        {/* <li>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+            className="nav__search"
+          ></input>
+          <button
+            type="submit"
+            className="nav__button"
+            onClick={buttonClickHandler}
+          >
+            <i className="fa fa-search" />
+          </button>
+        </li> */}
+        <li>
+          <NavLink to="/cart">cart</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard">Account</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 export default Navbar;
