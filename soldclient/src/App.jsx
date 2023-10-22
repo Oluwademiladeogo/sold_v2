55eebf84fce706afb6961bd04eeb1cec14ac3c4d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-22 23:59:49 +0100

    added react router

--- soldclient/src/App.jsx
+++ soldclient/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
 import "./App.css";
 import CardGrid from "./components/CardGrid/CardGrid";
 import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
-import path from "path";
+import Cart from "./components/Cart/Cart";
+import Dashboard from "./components/Dashboard/Dashboard";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-    {/* <Login/> */}
-      {/* <Navbar token="true" /> */}
-      <CardGrid />
+      <Navbar token="true" />
+      <Routes>
+        <Route path="/" element={<CardGrid/>} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </>
   );
 }
 //use passport to do an isauth kind of thing. if so, do something
 // pass the isauth as a function into any component youd be expecting it in props
 export default App;
+//you can also export the routes you need from index.js file and import here
