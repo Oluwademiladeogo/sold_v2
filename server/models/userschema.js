919d1a796012a38169ba24cfaacfc935be46f40b
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-24 10:10:09 +0100

    chore: code refactor

--- server/models/userschema.js
+++ server/models/userschema.js
@@ -1,6 +1,12 @@
-const joi = require("joi")
+const joi = require("joi");
 module.exports = joi.object({
-    name: joi.string().min(4).max(100).required(),
-    email: joi.string().min(5).max(255).email().required(),
-    password: joi.string().min(8).max(255).required()
-})
+  name: joi.string().min(4).max(100).required(),
+  email: joi.string().min(5).max(255).email().required(),
+  password: joi
+    .string()
+    .min(8)
+    .max(255)
+    .required()
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/)
+    
+});
