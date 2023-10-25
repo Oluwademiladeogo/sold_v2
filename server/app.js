6aa8319a15be70405331a8fb550df2f43652cbca
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-25 14:21:58 +0100

    Cors issues fixed but will modify

--- server/app.js
+++ server/app.js
@@ -2,11 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+// const corsOptions = {
+//     origin: "*"
+// }
+const cartCors = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Allow requests from this specific origin
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next()
+  };
+  
+// Apply the custom CORS middleware to the /cart route
+  app.use("/cart", cartCors);
+  
 app.use(cors());
 app.use(express.json());
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
 //using classes and controllers soon
 app.use("/", (require("./routes/index")))
 app.use("/login", require("./routes/login"))
+app.use("/signup", require("./routes/signup"))
 app.use("/cart", require("./routes/cart"))
 
 module.exports = app
