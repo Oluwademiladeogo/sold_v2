355e65ddd0d628263a653031523494d3701e89e1
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-10-25 14:17:19 +0100

    finalized index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,10 @@ const getCardData = require("../helpers/getCardData");
 router.get("/", async(req, res) => {
   res.setHeader("Content-Type", "application/json");
   const cardData = await getCardData()
-  res.json(cardData);
+  const response = {
+          Status: 200,
+          items: cardData.rows    
+  }
+  res.json(response);
 });
 module.exports = router;
